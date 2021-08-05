30e1605bcb35d661e921c6c3f5375b723af97184
Sanjeevani198 <81439195+Sanjeevani198@users.noreply.github.com>
2021-08-05 16:09:59 +0530

    Update Build.sql

--- Build.sql
+++ Build.sql
@@ -2,7 +2,7 @@ Drop table Build;
 Create table Build(Build_id NCHAR(50),Build_Started_By NCHAR(50), Time_Started NCHAR(50),Time_Elapsed NCHAR(50),Branch NCHAR(50),Repository NCHAR(50),Build_Pass_Fail NCHAR(50))
 SELECT Build_id ,Build_Started_By , Time_Started,Time_Elapsed,Branch,Repository,Build_Pass_Fail FROM OPENROWSET(
     BULK  'Blob/output_Final.json',
-    DATA_SOURCE = 'MyAzureBlobStorage26',
+    DATA_SOURCE = 'MyAzureBlobStorage30',
      SINGLE_CLOB
      ) AS DataFile
  CROSS APPLY OPENJSON(BulkColumn)
@@ -10,7 +10,7 @@ SELECT Build_id ,Build_Started_By , Time_Started,Time_Elapsed,Branch,Repository,
 INSERT INTO Build with (TABLOCK) (Build_id ,Build_Started_By , Time_Started,Time_Elapsed,Branch,Repository,Build_Pass_Fail)
  SELECT Build_id ,Build_Started_By , Time_Started,Time_Elapsed,Branch,Repository,Build_Pass_Fail FROM OPENROWSET(
     BULK  'Blob/output_Final.json',
-    DATA_SOURCE = 'MyAzureBlobStorage26',
+    DATA_SOURCE = 'MyAzureBlobStorage30',
      SINGLE_CLOB
      ) AS DataFile
  CROSS APPLY OPENJSON(BulkColumn)
