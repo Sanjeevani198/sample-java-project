a5091a7ea0cd6e59c925d188ef152aee316edd1e
Sanjeevani198 <81439195+Sanjeevani198@users.noreply.github.com>
2021-08-05 16:31:19 +0530

    Update Build.sql

--- Build.sql
+++ Build.sql
@@ -1,17 +1,17 @@
 Drop table Build;
-Create table Build(Build_id NCHAR(50),Build_Started_By NCHAR(50), Time_Started NCHAR(50),Time_Elapsed NCHAR(50),Branch NCHAR(50),Repository NCHAR(50),Build_Pass_Fail NCHAR(50))
-SELECT Build_id ,Build_Started_By , Time_Started,Time_Elapsed,Branch,Repository,Build_Pass_Fail FROM OPENROWSET(
+Create table Build(Build_id NCHAR(50),[Status] NCHAR(50), Branch NCHAR(50),Start_Time NCHAR(50),End_Time NCHAR(50),Build_started_by NCHAR(50),Repository NCHAR(50))
+SELECT Build_id ,[Status] , Branch ,Start_Time,End_Time ,Build_started_by ,Repository FROM OPENROWSET(
     BULK  'Blob/output_Final.json',
-    DATA_SOURCE = 'MyAzureBlobStorage30',
+    DATA_SOURCE = 'MyAzureBlobStorage31',
      SINGLE_CLOB
      ) AS DataFile
  CROSS APPLY OPENJSON(BulkColumn)
- WITH (Build_id NCHAR(50),Build_Started_By NCHAR(50), Time_Started NCHAR(50),Time_Elapsed NCHAR(50),Branch NCHAR(50),Repository NCHAR(50),Build_Pass_Fail NCHAR(50)) as BuildDetail
-INSERT INTO Build with (TABLOCK) (Build_id ,Build_Started_By , Time_Started,Time_Elapsed,Branch,Repository,Build_Pass_Fail)
- SELECT Build_id ,Build_Started_By , Time_Started,Time_Elapsed,Branch,Repository,Build_Pass_Fail FROM OPENROWSET(
+ WITH (Build_id NCHAR(50),[Status] NCHAR(50), Branch NCHAR(50),Start_Time NCHAR(50),End_Time NCHAR(50),Build_started_by NCHAR(50),Repository NCHAR(50)) as BuildDetail
+INSERT INTO Build with (TABLOCK) (Build_id ,[Status] , Branch ,Start_Time,End_Time ,Build_started_by ,Repository)
+ SELECT Build_id ,[Status] , Branch ,Start_Time,End_Time ,Build_started_by ,Repository FROM OPENROWSET(
     BULK  'Blob/output_Final.json',
-    DATA_SOURCE = 'MyAzureBlobStorage30',
+    DATA_SOURCE = 'MyAzureBlobStorage31',
      SINGLE_CLOB
      ) AS DataFile
  CROSS APPLY OPENJSON(BulkColumn)
- WITH (Build_id NCHAR(50),Build_Started_By NCHAR(50), Time_Started NCHAR(50),Time_Elapsed NCHAR(50),Branch NCHAR(50),Repository NCHAR(50),Build_Pass_Fail NCHAR(50)) as BuildDetail
+ WITH (Build_id NCHAR(50),[Status] NCHAR(50), Branch NCHAR(50),Start_Time NCHAR(50),End_Time NCHAR(50),Build_started_by NCHAR(50),Repository NCHAR(50)) as BuildDetail
