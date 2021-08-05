c20c40ca9a71e856121d3f5897eb54418e15cc0c
Sanjeevani198 <81439195+Sanjeevani198@users.noreply.github.com>
2021-08-05 16:02:43 +0530

    Update Build.sql

--- Build.sql
+++ Build.sql
@@ -1,7 +1,7 @@
 Drop table Build;
 Create table Build(Build_id NCHAR(50),Build_Started_By NCHAR(50), Time_Started NCHAR(50),Time_Elapsed NCHAR(50),Branch NCHAR(50),Repository NCHAR(50),Build_Pass_Fail NCHAR(50))
 SELECT Build_id ,Build_Started_By , Time_Started,Time_Elapsed,Branch,Repository,Build_Pass_Fail FROM OPENROWSET(
-    BULK  'Blob/Build_Final.json',
+    BULK  'Blob/output_Final.json',
     DATA_SOURCE = 'MyAzureBlobStorage26',
      SINGLE_CLOB
      ) AS DataFile
@@ -9,7 +9,7 @@ SELECT Build_id ,Build_Started_By , Time_Started,Time_Elapsed,Branch,Repository,
  WITH (Build_id NCHAR(50),Build_Started_By NCHAR(50), Time_Started NCHAR(50),Time_Elapsed NCHAR(50),Branch NCHAR(50),Repository NCHAR(50),Build_Pass_Fail NCHAR(50)) as BuildDetail
 INSERT INTO Build with (TABLOCK) (Build_id ,Build_Started_By , Time_Started,Time_Elapsed,Branch,Repository,Build_Pass_Fail)
  SELECT Build_id ,Build_Started_By , Time_Started,Time_Elapsed,Branch,Repository,Build_Pass_Fail FROM OPENROWSET(
-    BULK  'Blob/Build_Final.json',
+    BULK  'Blob/output_Final.json',
     DATA_SOURCE = 'MyAzureBlobStorage26',
      SINGLE_CLOB
      ) AS DataFile
