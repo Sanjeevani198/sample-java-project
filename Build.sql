aa4cb1f8c1d167f35c61f4b6e4833bbe38b09d78
Sanjeevani198 <81439195+Sanjeevani198@users.noreply.github.com>
2021-09-02 17:13:38 +0530

    Update Build.sql

--- Build.sql
+++ Build.sql
@@ -2,7 +2,7 @@ Drop table Build;
 Create table Build(Build_id NCHAR(50),[Status] NCHAR(50), Branch NCHAR(50),Start_Time NCHAR(50),End_Time NCHAR(50),Build_started_by NCHAR(50),Repository NCHAR(50))
 SELECT Build_id ,[Status] , Branch ,Start_Time,End_Time ,Build_started_by ,Repository FROM OPENROWSET(
     BULK  'Blob/output_Final.json',
-    DATA_SOURCE = 'MyAzureBlobStorage31',
+    DATA_SOURCE = 'sqlblobstorageaccount',
      SINGLE_CLOB
      ) AS DataFile
  CROSS APPLY OPENJSON(BulkColumn)
@@ -10,7 +10,7 @@ SELECT Build_id ,[Status] , Branch ,Start_Time,End_Time ,Build_started_by ,Repos
 INSERT INTO Build with (TABLOCK) (Build_id ,[Status] , Branch ,Start_Time,End_Time ,Build_started_by ,Repository)
  SELECT Build_id ,[Status] , Branch ,Start_Time,End_Time ,Build_started_by ,Repository FROM OPENROWSET(
     BULK  'Blob/output_Final.json',
-    DATA_SOURCE = 'MyAzureBlobStorage31',
+    DATA_SOURCE = 'sqlblobstorageaccount',
      SINGLE_CLOB
      ) AS DataFile
  CROSS APPLY OPENJSON(BulkColumn)
